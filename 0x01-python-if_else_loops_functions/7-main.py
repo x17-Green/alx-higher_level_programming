6fceba441b1a4ee4b6d53ae624b03ad11640f0cc
x17-green <x17.ebi@icloud.com>
2024-02-03 05:32:49 +0100

    7-islower.py Test file

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 islower = __import__('7-islower').islower
 
-print("Alphabeth \"g\" is {}".format("lowercase" if islower("g") else "uppercase"))
-print("Alphabeth \"R\" is {}".format("lowercase" if islower("R") else "uppercase"))
-print("Alphabeth \"3\" is {}".format("lowercase" if islower("3") else "uppercase"))
-print("Alphabeth \"e\" is {}".format("lowercase" if islower("e") else "uppercase"))
-print("Alphabeth \"N\" is {}".format("lowercase" if islower("N") else "uppercase"))
+print("Alphabet \"g\" is {}" .format(
+    "lowercase" if islower("g") else "uppercase"))
+print("Alphabet \"R\" is {}".format(
+    "lowercase" if islower("R") else "uppercase"))
+print("Alphabet \"3\" is {}".format(
+    "lowercase" if islower("3") else "uppercase"))
+print("Alphabet \"e\" is {}".format(
+    "lowercase" if islower("e") else "uppercase"))
+print("Alphabet \"N\" is {}".format(
+    "lowercase" if islower("N") else "uppercase"))
