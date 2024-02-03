1a81e2d7ddf915b41cbbc5f1a1e3af400713c939
x17-green <x17.ebi@icloud.com>
2024-02-03 04:25:55 +0100

    7-islower.py Test file

---
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+islower = __import__('7-islower').islower
+
+print("Alphabeth \"g\" is {}".format("lowercase" if islower("g") else "uppercase"))
+print("Alphabeth \"R\" is {}".format("lowercase" if islower("R") else "uppercase"))
+print("Alphabeth \"3\" is {}".format("lowercase" if islower("3") else "uppercase"))
+print("Alphabeth \"e\" is {}".format("lowercase" if islower("e") else "uppercase"))
+print("Alphabeth \"N\" is {}".format("lowercase" if islower("N") else "uppercase"))
