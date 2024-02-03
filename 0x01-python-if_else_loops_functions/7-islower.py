081dcd13f9e31dec23f093b0f9ded4db33a9f298
x17-green <x17.ebi@icloud.com>
2024-02-03 05:32:41 +0100

    islower function

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return c.isalpha() and ord(c) >= ord('a') and ord(c) <= ord('z')
+    if c.isalpha() and ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
