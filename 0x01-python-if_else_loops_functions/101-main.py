1a43b2af2d0dc1e30d66745458beb8bf5c65b5cd
x17-green <x17.ebi@icloud.com>
2024-02-04 16:14:16 +0100

    Test file for 101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+remove_char_at = __import__('101-remove_char_at').remove_char_at
+
+print(remove_char_at("Green House", 1))
+print(remove_char_at("Peretoru", 2))
+print(remove_char_at("I am fun!", 0))
+print(remove_char_at("Bank", 10))
+print(remove_char_at("Money", -2))
