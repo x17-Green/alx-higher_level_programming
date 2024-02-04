7a16ea26390a0298dea7b4584e85d0ae83ea8e31
x17-green <x17.ebi@icloud.com>
2024-02-04 14:57:46 +0100

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+	if number < 0:
+		last_n = number % -10
+		print(-(last_n), end='')
+	else:
+		last_n = number % 10
+		print(last_n, end='')
+		return abs(last_n)
+
