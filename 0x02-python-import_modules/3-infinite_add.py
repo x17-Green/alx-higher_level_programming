4c8c07bbb19723f94fd2b13882ce5e053a2dd5e1
x17-green <x17.ebi@icloud.com>
2024-05-01 06:34:25 +0100

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    ans = 0
 
-    if len(argv) == 1:
-        print("Error: input an int: {:d}".format(len(argv) - 1))
-    else:
-        for arg in argv[1:]:
-            ans += int(arg)
-        print("Total sum is = {}".format(ans))
+    ans: int = 0
+
+    for arg in argv[1:]:
+        ans += int(arg)
+    print("{:d}".format(ans))
