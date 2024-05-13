c039476da134b4d18df1d97e5e6140f6874e3b31
x17-green <x17.ebi@icloud.com>
2024-05-13 13:51:12 +0100

    Function to print list of integers...in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,11 +9,19 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
     None
     """
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if my_list is not None:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
     """
     Here's an explanation of the code:
 
-    We use a for loop to iterate over the my_list in reverse order. We start at the last index (len(my_list) - 1) and decrement by -1 until we reach the first index (-1).
-    For each iteration, we print the current element using str.format() to format the integer as a string.
+    The def print_reversed_list_integer(my_list=[]): function checks\
+    if value is None before trying to get its length. If value is None,\
+    it prints a message and skips the loop.
+
+    We use a for loop to iterate over the my_list in reverse\
+    order. We start at the last index (len(my_list) - 1) and\
+    decrement by -1 until we reach the first index (-1).
+    For each iteration, we print the current element using\
+    str.format() to format the integer as a string.
     """
