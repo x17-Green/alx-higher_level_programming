0657d774b290aa006df62efb40d023b8eb86894c
x17-green <x17.ebi@icloud.com>
2024-05-17 19:56:01 +0100

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 """
-    Write a class Square that defines a square by: (based on 4-square.py)
+    Write a class Square that defines a square by:\
+    (based on 4-square.py)
 
     Private instance attribute: size:
         property def size(self): to retrieve it
         property setter def size(self, value): to set it:
-            size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-            if size is less than 0, raise a ValueError exception with the message size must be >= 0
+            - size must be an integer, otherwise raise a TypeError\
+            exception with the message size must be an integer
+            - if size is less than 0, raise a ValueError exception\
+            with the message size must be >= 0
     Instantiation with optional size: def __init__(self, size=0):
-    Public instance method: def area(self): that returns the current square area
-    Public instance method: def my_print(self): that prints in stdout the square with the character #:
+    Public instance method: def area(self): that returns the\
+    current square area
+    Public instance method: def my_print(self): that prints\
+    in stdout the square with the character #:
         if size is equal to 0, print an empty line
     You are not allowed to import any module
-
 """
+
+
 class Square():
+
     @property
     def size(self):
         return self._size
