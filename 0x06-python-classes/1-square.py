4238458b7f241b4d723c674a3c82fc6f6553ac0c
x17-green <x17.ebi@icloud.com>
2024-05-17 21:51:40 +0100

    Updated doc string of class and method

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,6 +9,13 @@
 
 
 class Square():
+    """A class Square that defines a square by: (based on 0-square.py)
+    """
 
     def __init__(self, size):
+        """size __init__ method
+        
+        Private instance attribute: size
+        Instantiation with size (no type/value verification)
+        """
         self._size = size
