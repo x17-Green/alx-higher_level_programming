501b69001ca6edb74ddebc2d3bc607bf0158605d
x17-green <x17.ebi@icloud.com>
2024-05-17 20:13:26 +0100

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 """
-    Write a class Node that defines a node of a singly linked list by:
+    Write a class Node that defines a node of a singly linked\
+    list by:
 
     Private instance attribute: data:
         property def data(self): to retrieve it
         property setter def data(self, value): to set it:
-            data must be an integer, otherwise raise a TypeError exception with the message data must be an integer
+            - data must be an integer, otherwise raise a TypeError\
+            exception with the message data must be an integer
     Private instance attribute: next_node:
         property def next_node(self): to retrieve it
         property setter def next_node(self, value): to set it:
-            next_node can be None or must be a Node, otherwise raise a TypeError exception with the message next_node must be a Node object
-    Instantiation with data and next_node: def __init__(self, data, next_node=None):
+            - next_node can be None or must be a Node, otherwise\
+            raise a TypeError exception with the message next_node\
+            must be a Node object
+    Instantiation with data and next_node:\
+    def __init__(self, data, next_node=None):
 
-And, write a class SinglyLinkedList that defines a singly linked list by:
+And, write a class SinglyLinkedList that defines a singly linked\
+list by:
 
     Private instance attribute: head (no setter or getter)
     Simple instantiation: def __init__(self):
     Should be printable:
         print the entire list in stdout
         one node number by line
-    Public instance method: def sorted_insert(self, value): that inserts a new Node into the correct sorted position in the list (increasing order)
+    Public instance method: def sorted_insert(self, value):\
+    that inserts a new Node into the correct sorted position\
+    in the list (increasing order)
     You are not allowed to import any module
-
 """
+
+
 class Node():
+
     def _init_(self, data, next_node=None):
         self._data = data
+
     @property
     def data(self):
         return self._data
@@ -37,6 +48,7 @@ class Node():
             raise TypeError("data must be an integer")
 
         self._next_node = next_node
+
     @property
     def next_node(self):
         return self._next_node
@@ -47,7 +59,9 @@ class Node():
         if next_node is None or None:
             raise TypeError("next_node must be a Node object")
 
+
 class SinglyLinkedList():
+
     def _init_(self):
         self._head = head
 
