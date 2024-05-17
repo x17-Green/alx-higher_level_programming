23277ce6c6682a182d942257f92ebb7da15a2316
x17-green <x17.ebi@icloud.com>
2024-05-17 19:42:43 +0100

    Square with size: A class that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,7 @@
     (based on 0-square.py)
     Private instance attribute: size
     Instantiation with size (no type/value verification)
-    You are not allowed to import any module
+    You are not allowed to import any module.
 """
 
 
