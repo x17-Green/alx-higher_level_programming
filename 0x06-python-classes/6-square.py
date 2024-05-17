b87b9f0a41cf844d0cc0757db484064648fd5e01
x17-green <x17.ebi@icloud.com>
2024-05-17 20:04:30 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,24 +1,35 @@
 """
-    Write a class Square that defines a square by: (based on 5-square.py)
+    Write a class Square that defines a square by:\
+    (based on 5-square.py)
 
     Private instance attribute: size:
         property def size(self): to retrieve it
         property setter def size(self, value): to set it:
-            size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-            if size is less than 0, raise a ValueError exception with the message size must be >= 0
+            - size must be an integer, otherwise raise a TypeError\
+            exception with the message size must be an integer
+            - if size is less than 0, raise a ValueError exception\
+            with the message size must be >= 0
     Private instance attribute: position:
         property def position(self): to retrieve it
         property setter def position(self, value): to set it:
-            position must be a tuple of 2 positive integers, otherwise raise a TypeError exception with the message position must be a tuple of 2 positive integers
-    Instantiation with optional size and optional position: def __init__(self, size=0, position=(0, 0)):
-    Public instance method: def area(self): that returns the current square area
-    Public instance method: def my_print(self): that prints in stdout the square with the character #:
+            - position must be a tuple of 2 positive integers,\
+            otherwise raise a TypeError exception with the message\
+            position must be a tuple of 2 positive integers
+    Instantiation with optional size and optional position:\
+    def __init__(self, size=0, position=(0, 0)):
+    Public instance method: def area(self): that returns the current\
+    square area
+    Public instance method: def my_print(self): that prints in stdout\
+    the square with the character #:
         if size is equal to 0, print an empty line
-        position should be use by using space - Don’t fill lines by spaces when position[1] > 0
+        position should be use by using space - Don’t fill lines by\
+        spaces when position[1] > 0
     You are not allowed to import any module
-
 """
+
+
 class Square():
+
     def __init__(self, size=0, position=(0, 0)):
         self._size = size
         if type(size) is not int:
@@ -36,7 +47,8 @@ class Square():
 
         self._position = position
         if type(position) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive\
+                            integers")
 
     @property
     def position(self):
@@ -55,4 +67,3 @@ class Square():
         area = self.size
         for i in range(area):
             print('#' * area)
-
