dbe095743447c14050b8afe0d2e25a0e700b09d6
x17-green <x17.ebi@icloud.com>
2024-05-24 12:54:13 +0100

    Updated size variation funtion

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,8 +14,19 @@
 
 
 class Square():
+    """ Write a class Square that defines a square\
+        by: (based on 1-square.py)
+    """
 
     def __init__(self, size=0):
+        """ size __init__ method
+         
+        Private instance attribute: size
+        Instantiation with optional size: def __init__(self, size=0):
+          - size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+          - if size is less than 0, raise a ValueError exception with the message size must be >= 0
+        You are not allowed to import any module
+        """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
